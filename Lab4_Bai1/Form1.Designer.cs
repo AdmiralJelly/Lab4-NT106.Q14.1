c2bc625fec875bd4af2c439c1f980cda83ec9fe6
ngovantinh-ops-pwn <ngovantinh27092005@gmail.com>
2025-12-28 13:29:21 +0700

    bai 1 2 3

---
+++ Lab4_Bai1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Lab4_Bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUrl = new TextBox();
+            btnGet = new Button();
+            rtbContent = new RichTextBox();
+            SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            txtUrl.Location = new Point(12, 15);
+            txtUrl.Margin = new Padding(3, 4, 3, 4);
+            txtUrl.Name = "txtUrl";
+            txtUrl.Size = new Size(650, 27);
+            txtUrl.TabIndex = 0;
+            txtUrl.Text = "";
+            // 
+            // btnGet
+            // 
+            btnGet.Location = new Point(680, 12);
+            btnGet.Margin = new Padding(3, 4, 3, 4);
+            btnGet.Name = "btnGet";
+            btnGet.Size = new Size(90, 32);
+            btnGet.TabIndex = 1;
+            btnGet.Text = "GET";
+            btnGet.UseVisualStyleBackColor = true;
+            btnGet.Click += btnGet_Click;
+            // 
+            // rtbContent
+            // 
+            rtbContent.Location = new Point(12, 62);
+            rtbContent.Margin = new Padding(3, 4, 3, 4);
+            rtbContent.Name = "rtbContent";
+            rtbContent.Size = new Size(758, 562);
+            rtbContent.TabIndex = 2;
+            rtbContent.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(782, 641);
+            Controls.Add(rtbContent);
+            Controls.Add(btnGet);
+            Controls.Add(txtUrl);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Bai 1";
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.RichTextBox rtbContent;
+    }
+}
---
+++ Lab4_Bai1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// 
+using System.Net;
+using System.IO;
+
+namespace Lab4_Bai1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Hàm lấy nội dung HTML từ URL (Dựa trên tài liệu trang 8)
+        private string getHTML(string szURL)
+        {
+
+            WebRequest request = WebRequest.Create(szURL);
+
+ 
+            WebResponse response = request.GetResponse();
+
+
+            Stream dataStream = response.GetResponseStream();
+
+            StreamReader reader = new StreamReader(dataStream);
+
+
+            string responseFromServer = reader.ReadToEnd();
+
+            response.Close();
+
+            return responseFromServer; 
+        }
+
+        // Sự kiện khi nhấn nút GET
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Lấy đường dẫn từ ô TextBox
+                string url = txtUrl.Text;
+
+                // Gọi hàm xử lý và hiển thị kết quả ra RichTextBox
+                string htmlContent = getHTML(url);
+                rtbContent.Text = htmlContent;
+            }
+            catch (Exception ex)
+            {
+                // Thông báo nếu có lỗi (ví dụ: sai đường dẫn, mất mạng)
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+        }
+    }
+}
